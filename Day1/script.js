cb8621347545db5821270bc113f5ac5cf9f38237
lozukka <lotsulotsu@gmail.com>
2025-12-12 10:58:52 +0200

    Update script.js
    Solution done for part 1 and part 2

--- Day1/script.js
+++ Day1/script.js
@@ -1,6 +1,7 @@
 // fetch the input.txt that contains the input for the lock
 let sum = 50;
 let ammountOfZeros = 0;
+
 fetch("input.txt")
   .then((response) => {
     if (!response.ok) {
@@ -10,20 +11,18 @@ fetch("input.txt")
   })
   .then((text) => {
     const inputArray = text.split("\r\n"); // string to array
-    countSum(inputArray);
+    countSum(inputArray); //part 1
+    countSumPart2(inputArray); //part 2
   })
   .catch((err) => {
     console.error(err);
   });
-
+//part 1 solution
 function countSum(inputArray) {
   inputArray.forEach((element) => {
     //check if the input number is more than 99
     let modifyedNum = parseInt(element.slice(1));
     if (modifyedNum > 99) {
-      //part 2 of the puzzle:
-      ammountOfZeros = ammountOfZeros + Math.floor(modifyedNum / 100);
-      console.log("amount of zeros: " + ammountOfZeros);
       modifyedNum = modifyedNum % 100;
     }
     //check wich way to turn the dial, and then add or substract the input
@@ -31,25 +30,42 @@ function countSum(inputArray) {
       sum = sum + modifyedNum;
       if (sum > 99) {
         sum = sum % 100;
-        //part 2
-        //ammountOfZeros++;
-        console.log("amount of zeros: " + ammountOfZeros);
       }
     } else {
       sum = sum - modifyedNum;
       if (sum < 0) {
         sum = 100 - Math.abs(sum);
-        //part 2
-        //ammountOfZeros++;
-        console.log("amount of zeros: " + ammountOfZeros);
       }
     }
     if (sum == 0) {
       ammountOfZeros++;
-      console.log("amount of zeros: " + ammountOfZeros);
+      //console.log("amount of zeros: " + ammountOfZeros);
     }
 
-    console.log(sum);
+    //console.log(sum);
   });
-  console.log("total: " + ammountOfZeros);
+  console.log("Total 1: " + ammountOfZeros);
+}
+
+//part 2 solution
+let sum2 = 50;
+let ammountOfZeros2 = 0;
+
+function countSumPart2(inputArray) {
+  inputArray.forEach((element) => {
+    let distance = parseInt(element.slice(1));
+
+    for (let i = 0; i < distance; i++) {
+      if (element.charAt(0) === "R") {
+        sum2 = (((sum2 + 1) % 100) + 100) % 100;
+      } else {
+        sum2 = (((sum2 - 1) % 100) + 100) % 100;
+      }
+      if (sum2 == 0) {
+        ammountOfZeros2++;
+        //console.log("ammount of zeros 2: " + ammountOfZeros2);
+      }
+    }
+  });
+  console.log("Total 2: " + ammountOfZeros2);
 }
